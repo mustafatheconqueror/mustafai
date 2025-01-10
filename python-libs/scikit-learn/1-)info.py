020cb34da00137757542242d5422965d66030ba9
muskaracabey <muskaracabey@hepsiburada.com>
2025-01-10 18:26:28 +0300

    a

--- python-libs/scikit-learn/1-)info.py
+++ python-libs/scikit-learn/1-)info.py
@@ -3,5 +3,5 @@
     - Scikit-learn is a Python library for machine learning.
     - It features various classification, regression and clustering algorithms including support vector machines, random forests, 
     - gradient boosting, k-means and DBSCAN, and is designed to interoperate with the Python numerical and scientific libraries NumPy and SciPy.
-
+s
 """
